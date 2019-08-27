85e0ce437cb113494f84e3f35eba97febe280904
odanielbispo <danieubispo@gmail.com>
2019-08-27 10:48:26 -0300

    exercicios

--- javaiscripiti.js
+++ javaiscripiti.js
@@ -128,3 +128,45 @@ function ex7(){
 	}
 
 }
+
+function ex22(){
+	var idade = Number(document.getElementById("idade").value);
+	var peso = Number(document.getElementById("peso").value);
+	var risco = 0
+
+	if (idade <= 20) {
+		if (peso < 61){
+			risco = 9
+		}
+		else if (peso <= 90){
+			risco = 8 
+		}
+		else{
+			risco = 7
+		}
+	
+	}
+	else if (idade < 50){
+		if (peso < 61){
+			risco = 6
+		}
+		else if (peso <= 90){
+			risco = 5 
+		}
+		else{
+			risco = 4
+		}
+	}
+	else{
+		if (peso < 61){
+			risco = 3
+		}
+		else if (peso <= 90){
+			risco = 2 
+		}
+		else{
+			risco = 1
+		}
+}
+	alert("O risco é: " + risco)
+}
